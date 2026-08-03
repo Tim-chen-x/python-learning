3ae9be80329c8f5408330a048f99c4f568b3e1ff
Tim-chen-x <xxxcv0662@gmail.com>
2026-08-03 15:17:01 +0800

    8.3

--- 第二章/08.for循环.py
+++ 第二章/08.for循环.py
@@ -1,6 +1,20 @@
-msg = input("请输入需要遍历的字符串：")
+# msg = input("请输入需要遍历的字符串：")
+#
+# for s in msg:
+#     print(f"元素:{s}")
+# else :
+#     print("循环结束")
 
-for s in msg:
-    print(f"元素:{s}")
-else :
-    print("循环结束")
+
+
+#print(*) 自带换行效果，每一次输出都会另起一行
+#print("*",end="")end表示的是每一次输出以什么结束;默认 \n,表示换行
+#1.接受键盘录入
+m = int(input("请输入长方形的长度:"))
+n = int(input("请输入长方形的宽度:"))
+
+#2.打印长方形
+for j in range(n):
+    for i in range(m):
+        print("*",end="  ")
+    print()
